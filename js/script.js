aaf4fdbd59802a98a0c2467651317ba262c35354
Simon Mazurek <dev.mazureq@gmail.com>
2023-02-10 16:01:22 +0100

    add new variables to scripts.js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let computerMove = `Kamień`;
+let playerMove= 'Papier';
+printMessage('Zagrałem '+ computerMove +'! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Gracz zagrał ' +playerMove + '! Rundę wygrywa GRACZ!');
